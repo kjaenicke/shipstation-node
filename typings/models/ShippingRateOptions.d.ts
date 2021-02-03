6a531553371f5778f12eaa30187730ef5ded1839
David Knape <github@bumpslide.com>
2021-02-02 23:01:36 -0800

    Added shipping rates

---
+++ typings/models/ShippingRateOptions.d.ts
@@ -0,0 +1,17 @@
+import { IDimensions } from "./Dimensions";
+import { IWeight } from "./Weight";
+export declare type DeliveryConfirmation = 'none' | 'delivery' | 'signature' | 'adult_signature' | 'direct_signature';
+export interface IShippingRateOptions {
+    carrierCode: string;
+    serviceCode?: null | string;
+    packageCode?: null | string;
+    fromPostalCode: string;
+    toState?: null | string;
+    toCountry: string;
+    toPostalCode: string;
+    toCity?: null | string;
+    weight: IWeight;
+    dimensions?: null | IDimensions;
+    confirmation?: DeliveryConfirmation;
+    residential?: boolean;
+}
