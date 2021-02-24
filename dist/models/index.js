d88e6efd6b88f36c8cb2a2b7c4e934df0c019931
Keenan Jaenicke <kjaenick@gmail.com>
2021-02-24 16:56:52 -0500

    build

--- dist/models/index.js
+++ dist/models/index.js
@@ -1,28 +1,2 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./Address"), exports);
-__exportStar(require("./AdvancedOptions"), exports);
-__exportStar(require("./Carrier"), exports);
-__exportStar(require("./Dimensions"), exports);
-__exportStar(require("./Fulfillment"), exports);
-__exportStar(require("./InsuranceOptions"), exports);
-__exportStar(require("./InternationalOptions"), exports);
-__exportStar(require("./Order"), exports);
-__exportStar(require("./Pagination"), exports);
-__exportStar(require("./Shipment"), exports);
-__exportStar(require("./ShippingRate"), exports);
-__exportStar(require("./ShippingRateOptions"), exports);
-__exportStar(require("./Store"), exports);
-__exportStar(require("./Warehouse"), exports);
-__exportStar(require("./Webhook"), exports);
-__exportStar(require("./Weight"), exports);
